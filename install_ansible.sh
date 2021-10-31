9d8b50e8c27c38d10e003bdc45b60936899e3ce0
Bradford Wagner <bwagner@blackrock.com>
2021-10-31 00:15:10 -0400

    update

--- install_ansible.sh
+++ install_ansible.sh
@@ -1,14 +1,13 @@
 function alpine() {
-  echo hello alpine
   apk add git \
           ansible
 }
 
 function ubuntu() {
   echo hello ubuntu
-  apt update
-  apt install software-properties-common -y
-  add-apt-repository -y --update ppa:ansible/ansible
-  apt install ansible -y
+# apt update
+# apt install software-properties-common -y
+# add-apt-repository -y --update ppa:ansible/ansible
+# apt install ansible -y
 }
 
