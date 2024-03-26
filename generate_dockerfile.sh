003f817087fad094a0e156767775e3f37d4b80b5
Bradford Wagner <wagner.bradford@gmail.com>
2024-03-25 21:44:46 -0400

    gh actions (#7)
    * gh actions
    
    * invoke sh
    
    * use . to source
    
    * ++ bullseye
    
    * fix os
    
    * ++ ubuntu
    
    * align

---
+++ generate_dockerfile.sh
@@ -0,0 +1,7 @@
+# this is used to generate a docker file in the build task
+# input 0 is expected to be the nth upstream item
+v=$1
+os=$(echo ${v} | jq -r '.os')
+cat <<FOF
+RUN ./install_ansible.sh ${os}
+FOF
