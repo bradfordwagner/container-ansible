4319ca91e975d25e90ef3d5bfa918972b628705e
bradfordwagner <wagner.bradford@gmail.com>
2024-04-08 22:26:33 -0400

    pkg_installer=rhel

--- install_ansible.sh
+++ install_ansible.sh
@@ -37,6 +37,17 @@ debian_deps() {
     git
 }
 
+rhel_deps() {
+  yum update -y
+  yum install -y \
+    gcc \
+    python3 \
+    python3-devel \
+    python3-virtualenv \
+    wget \
+    git
+}
+
 ${pkg_installer}_deps
 
 cd /
