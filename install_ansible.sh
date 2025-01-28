29e8e790be720de1ab58db4689622d2c1be8c0de
Bradford Wagner <wagner.bradford@gmail.com>
2025-01-28 09:32:44 -0500

    feat: use dagger container builds module (#20)
    * feat(dagger): 0.13
    
    * rhel_python=3.12.8
    
    * add python versions doc
    
    * remove rocky 8
    
    * comment

--- install_ansible.sh
+++ install_ansible.sh
@@ -59,8 +59,13 @@ rhel_deps() {
     rsync \
     zlib-devel
 
+  # I removed rocky 8 for this error:
+  # 97  : │ [6m9s] | fatal: [localhost]: FAILED! => {"changed": false, "module_stderr": "Traceback (most recent call last):\n  File \"<stdin>\", line 12, in <module>\n  File \"<frozen importlib._bootstrap>\", line 971, in _find_and_load\n  File \"<frozen importlib._bootstrap>\", line 951, in _find_and_load_unlocked\n  File \"<frozen importlib._bootstrap>\", line 894, in _find_spec\n  File \"<frozen importlib._bootstrap_external>\", line 1157, in find_spec\n  File \"<frozen importlib._bootstrap_external>\", line 1131, in _get_spec\n  File \"<frozen importlib._bootstrap_external>\", line 1112, in _legacy_get_spec\n  File \"<frozen importlib._bootstrap>\", line 441, in spec_from_loader\n  File \"<frozen importlib._bootstrap_external>\", line 544, in spec_from_file_location\n  File \"/tmp/ansible_ansible.legacy.dnf_payload_z8v1z445/ansible_ansible.legacy.dnf_payload.zip/ansible/module_utils/basic.py\", line 5\nSyntaxError: future feature annotations is not defined\n", "module_stdout": "", "msg": "MODULE FAILURE\nSee stdout/stderr for the exact error", "rc": 1}
+# likely because of wrong python version being picked up. need to circle back on this
+
   # maybe we should be installing python for all os
-  python_verson=3.12.7
+  # https://www.python.org/doc/versions/
+  python_verson=3.12.8
   wget https://www.python.org/ftp/python/${python_verson}/Python-${python_verson}.tar.xz
   xz -d -v Python-${python_verson}.tar.xz
   tar xf Python-${python_verson}.tar
