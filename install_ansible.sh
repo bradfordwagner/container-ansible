c3306b9840c996ed2d47b0cad1cfbea3ec596ace
Bradford Wagner <wagner.bradford@gmail.com>
2026-01-26 14:23:58 -0500

    feat: base=4.3.0,ansible_version=12.3.0,python_verson=3.12.12 (#23)
    * feat: base=4.2.1
    
    * python_verson=3.12.12,ansible_version=13.2.0
    
    * ansible_version=12.3.0
    
    * remove jammy
    
    * chore(os): alpine=3.22,3.23 debian=bookworm,bookworm-slim,trixie,trixie-slim ubuntu=noble,plucky archlinux=latest
    
    * base=4.3.0

--- install_ansible.sh
+++ install_ansible.sh
@@ -65,7 +65,7 @@ rhel_deps() {
 
   # maybe we should be installing python for all os
   # https://www.python.org/doc/versions/
-  python_verson=3.12.8
+  python_verson=3.12.12
   wget https://www.python.org/ftp/python/${python_verson}/Python-${python_verson}.tar.xz
   xz -d -v Python-${python_verson}.tar.xz
   tar xf Python-${python_verson}.tar
@@ -96,7 +96,7 @@ python3 get-pip.py
 
 # ansible + ansible core versions
 # https://docs.ansible.com/ansible/latest/reference_appendices/release_and_maintenance.html#ansible-community-changelogs
-ansible_version=10.7.0
+ansible_version=12.3.0
 python3 -m pip install ansible==${ansible_version}
 
 # smoketest
